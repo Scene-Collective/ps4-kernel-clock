a31c4f0c0c21c4f458ba0e8e6d073212842db94f
Al-Azif <alazif@miskatonic.io>
2021-01-23 16:15:55 -0800

    Add debug socket

--- source/main.c
+++ source/main.c
@@ -1,3 +1,7 @@
+//#define DEBUG_SOCKET
+#define DEBUG_IP "192.168.2.2"
+#define DEBUG_PORT 9023
+
 #include "ps4.h"
 
 int _main(struct thread *td) {
@@ -6,12 +10,22 @@ int _main(struct thread *td) {
   initKernel();
   initLibc();
 
+#ifdef DEBUG_SOCKET
+  initNetwork();
+  DEBUG_SOCK = SckConnect(DEBUG_IP, DEBUG_PORT);
+#endif
+
   jailbreak();
   kernel_clock(14861963);
 
   initSysUtil();
 
   printf_notification("Kernel time changed!");
+
+#ifdef DEBUG_SOCKET
+  printf_socket("\nClosing socket...\n\n");
+  SckClose(DEBUG_SOCK);
+#endif
 
   return 0;
 }
