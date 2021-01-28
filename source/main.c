47d1fbae5d413f7049701e9c907c17757e0b3d0a
Al-Azif <alazif@miskatonic.io>
2021-01-27 17:49:22 -0800

    Change "socket" to "debug"

--- source/main.c
+++ source/main.c
@@ -23,7 +23,7 @@ int _main(struct thread *td) {
   printf_notification("Kernel time changed!");
 
 #ifdef DEBUG_SOCKET
-  printf_socket("\nClosing socket...\n\n");
+  printf_debug("Closing socket...\n");
   SckClose(DEBUG_SOCK);
 #endif
 
